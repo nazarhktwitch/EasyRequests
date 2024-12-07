4d61cedb38f3f768e351a2690530a37ba32241ff
NazarHK <nazarburlan2@gmail.com>
2024-12-07 19:32:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from cx_Freeze import setup, Executable
 
+includes = ["queue"]
+
 setup(
     name="EasyRequests",
     version="0.1",
     description="Easy python requests",
+    options={
+        "build_exe": {
+            "packages": ["requests", "json"],
+            "includes": includes,
+        }
+    },
     executables=[Executable("EasyRequests.py")]
 )
